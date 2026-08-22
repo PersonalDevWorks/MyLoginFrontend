259cd3a234a15b2b1771602708ba93d6bb255fd4
Dilushkumar Vasudevan <dilushvasu1996@gmail.com>
2026-08-22 21:29:16 +0530

    Login Page created with client side validation

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,8 +1,70 @@
+import { useState, type SubmitEvent } from 'react';
+
 export default function Login() {
+
+  const [error, setError] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  async function handleSubmit(event: SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setError(null);
+    setIsSubmitting(true);
+    //console.log(event.target);
+    //console.log(event.currentTarget);
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get('username')?.toString() || '';
+    const password = formData.get('password')?.toString() || '';
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    try {
+      const response = await fetch('http://localhost:3000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        setError(errorData.error || 'Login failed. Please try again.');
+        return;
+      }
+
+      const data = await response.json();
+      console.log('Login successful:', data);
+
+    }catch (error) {
+      console.error('Error during login:', error);
+      setError('Network error. Please try again later.');
+    }finally {
+      setIsSubmitting(false);
+    }
+
+  }
+
+
   return (
     <div>
-      <h1>Login</h1>
-      <p>Please enter your credentials to log in.</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:
+            <input type="email" id="username" name="username" autoComplete="username" required />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">Password:
+            <input type="password" id="password" name="password" autoComplete="current-password" required />
+          </label>
+        </div>
+        {error && <div style={{ color: 'red' }}>{error}</div>}
+        <div>
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? 'Logging in...' : 'Login'}
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
