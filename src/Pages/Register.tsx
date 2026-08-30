77b3de03a97deba6655b7adee6c031e46a9c508b
Dilushkumar Vasudevan <dilushvasu1996@gmail.com>
2026-08-30 12:06:46 +0530

    modify the backend port number

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -85,7 +85,7 @@ export default function Register() {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:3000/api/register', {
+      const response = await fetch('http://localhost:8080/api/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
